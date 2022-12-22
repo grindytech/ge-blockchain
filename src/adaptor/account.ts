6896d60c447bbcdc48c9a750b8996165ed3901a6
stefan muto <mutobui@gmail.com>
2022-12-22 16:16:58 +0700

    ge account

--- src/adaptor/account.ts
+++ src/adaptor/account.ts
@@ -1,5 +1,5 @@
 import { GEAccount, GETransaction } from "../types";
 
 export interface GEAccountAdaptor {
-    get_ge_account(transaction_data: GETransaction): Promise<GEAccount[]>;
+    get_ge_accounts(transaction_data: GETransaction): Promise<GEAccount[]>;
 }
