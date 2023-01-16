c13c0791772acf126e7301dd99e6d6f33adad541
stefan muto <mutobui@gmail.com>
2023-01-16 14:54:18 +0700

    add time to acc

--- dist/types/account.d.ts
+++ dist/types/account.d.ts
@@ -1,7 +1,8 @@
-import { Address, Balance, Hash, Height } from "./types";
+import { Address, Balance, Hash, Height, Timestamp } from "./types";
 export interface GEAccount {
     address: Address;
     block_height: Height;
+    timestamp: Timestamp;
     transaction: Hash;
     balance: Balance;
 }
--- src/types/account.ts
+++ src/types/account.ts
@@ -1,8 +1,9 @@
-import { Address, Balance, Hash, Height } from "./types";
+import { Address, Balance, Hash, Height, Timestamp } from "./types";
 
 export interface GEAccount {
     address: Address;
-    block_height: Height; 
+    block_height: Height;
+    timestamp: Timestamp;
     transaction: Hash;
     balance: Balance;
 }
