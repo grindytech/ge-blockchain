e1d2cff760a7922d3ce45ed62956e98f0c86570c
bao_cryptoviet <baonguyen120301@gmail.com>
2023-02-01 13:57:39 +0700

    add new properties

--- src/types/account.ts
+++ src/types/account.ts
@@ -1,16 +1,17 @@
 import { Address, Balance, Hash, Height, Timestamp } from "./types";
 
 export interface GEAccount {
-    address: Address;
-    block_height: Height;
-    timestamp: Timestamp;
-    transaction: Hash;
-    balance: Balance;
+  address: Address;
+  block_height: Height;
+  timestamp: Timestamp;
+  transaction: Hash;
+  balance: Balance;
 }
 
 export type GEAccountGQL = {
   address: Address;
   blockHeight: Height;
+  timestamp: Timestamp;
   transaction: Hash;
   balance: Balance;
 };
