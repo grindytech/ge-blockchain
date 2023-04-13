6a1a0426e3eabaedb235a7d197cf746dcc67e60b
bao_cryptoviet <baonguyen120301@gmail.com>
2023-04-13 12:00:15 +0700

    refactor account adaptor

--- src/adaptor/account.ts
+++ src/adaptor/account.ts
@@ -1,9 +1,22 @@
+import { Height } from "../types";
+
 export interface GEAccountAdaptor<TAccount, TTransaction> {
   /**
    * Returns account data from tx
    *
+   * @param block_number - current block number
    * @param txs_data - list of transaction Datas
    * @returns GE account data
    */
-  get_ge_accounts(txs_data: TTransaction[]): Promise<TAccount[]>;
+  get_ge_accounts(
+    block_number: Height,
+    txs_data: TTransaction[]
+  ): Promise<TAccount[]>;
+
+  /**
+   * Returns latest block height
+   *
+   * @returns block height
+   */
+  get_block_number(): Promise<Height>;
 }
