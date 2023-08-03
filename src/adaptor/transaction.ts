4f0ac0c583cfd35304824e0e29d473af80d8d990
bao_cryptoviet <baonguyen120301@gmail.com>
2023-08-03 13:57:47 +0700

    add block state

--- src/adaptor/transaction.ts
+++ src/adaptor/transaction.ts
@@ -2,10 +2,21 @@ import { Hash } from "../types";
 
 export interface GETransactionAdaptor<TTransaction> {
   /**
+   * Returns latest block height
+   *
+   * @param block - block number
+   * @returns block state
+   */
+  get_block_state(block: number): Promise<string>;
+
+  /**
    * Returns transaction data
    *
    * @param tx_hashes - list of transaction hashes
    * @returns GE transaction data
    */
-  to_ge_transaction(tx_hashes: Hash[]): Promise<TTransaction[]>;
+  to_ge_transaction(
+    tx_hashes: Hash[],
+    block_state?: string
+  ): Promise<TTransaction[]>;
 }
